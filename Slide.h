650e1dda8a9370b30c455acc1be5697e434f8c09
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-13 02:11:14 +0900

    update : v2만들어둠 일단

--- Slide.h
+++ Slide.h
@@ -46,6 +46,64 @@ void handleModelSlice1(Model& originalModel, const glm::vec3& planeNormal, float
         read_obj_file("obj/teapot3.obj", bottomPart, "teapot");
     }
 
+    // 원래 모델을 장면에서 제거
+    removeModelFromWorld(models, originalModel);
+    InitBuffer();
+
+    // 분할된 두 모델을 추가
+
+    addModelToPhysicsWorld(topPart);
+    models.push_back(topPart);
+    AddModelBuffer(topPart);
+
+    addModelToPhysicsWorld(bottomPart);
+    models.push_back(bottomPart);
+    AddModelBuffer(bottomPart);
+}
+
+void handleModelSlice2(Model& originalModel, const glm::vec3& planeNormal, float planeOffset, std::vector<Model>& models,
+    void (*addModelToPhysicsWorld)(Model& model), void (*AddModelBuffer)(const Model& model), void (*removeModelFromWorld)(std::vector<Model>& models, Model& modelToDelete), void(*InitBuffer)(), glm::vec3 dragSqu[4]) {
+    Model topPart = originalModel, bottomPart = originalModel;
+    topPart.vertices.clear();
+    topPart.faces.clear();
+    topPart.rigidBody = nullptr;
+    bottomPart.vertices.clear();
+    bottomPart.faces.clear();
+    bottomPart.rigidBody = nullptr;
+    // dragSqu[0]과 dragSqu[2]를 이용하여 직선의 기울기와 절편 계산
+    float slope = (dragSqu[2].y - dragSqu[0].y) / (dragSqu[2].x - dragSqu[0].x);  // 기울기 m
+    float intercept = dragSqu[0].y - slope * dragSqu[0].x;                        // 절편 b
+
+    for (const Face& face : originalModel.faces) {
+        // 각 face의 세 꼭짓점 가져오기
+        Vertex v0 = originalModel.vertices[face.v1];
+        Vertex v1 = originalModel.vertices[face.v2];
+        Vertex v2 = originalModel.vertices[face.v3];
+
+        // 삼각형의 중심점을 계산
+        float centerX = (v0.x + v1.x + v2.x) / 3.0f;
+        float centerY = (v0.y + v1.y + v2.y) / 3.0f;
+
+        // 중심점이 직선 위에 있는지 아래에 있는지 확인
+        // 직선의 방정식: y = slope * x + intercept
+        float lineY = slope * centerX + intercept;
+
+        if (centerY >= lineY) {
+            // 삼각형의 중심이 직선 위쪽에 있는 경우 topPart로 분류
+            topPart.faces.push_back(face);
+            topPart.vertices.push_back(v0);
+            topPart.vertices.push_back(v1);
+            topPart.vertices.push_back(v2);
+        }
+        else {
+            // 삼각형의 중심이 직선 아래쪽에 있는 경우 bottomPart로 분류
+            bottomPart.faces.push_back(face);
+            bottomPart.vertices.push_back(v0);
+            bottomPart.vertices.push_back(v1);
+            bottomPart.vertices.push_back(v2);
+        }
+    }
+
 
 
     // 원래 모델을 장면에서 제거
@@ -53,7 +111,6 @@ void handleModelSlice1(Model& originalModel, const glm::vec3& planeNormal, float
     InitBuffer();
 
     // 분할된 두 모델을 추가
-
     addModelToPhysicsWorld(topPart);
     models.push_back(topPart);
     AddModelBuffer(topPart);
